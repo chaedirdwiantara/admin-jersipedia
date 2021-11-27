6ea66059e68555bc06c86473d99c4ce7bf82b2fa
chaedirdwiantara <chaedirdwiantara@gmail.com>
2021-11-27 11:23:57 +0700

    initial commit

---
+++ src/config/FIREBASE/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyA7DdsmGrXkqF_tHfkvyGZRk1v9oZKqF1s',
+  authDomain: 'jersipedia-5c7b5.firebaseapp.com',
+  projectId: 'jersipedia-5c7b5',
+  storageBucket: 'jersipedia-5c7b5.appspot.com',
+  messagingSenderId: '256948275405',
+  appId: '1:256948275405:web:490a41faed70c72ef08184',
+});
+
+const FIREBASE = firebase;
+
+export default FIREBASE;
---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from "redux";
+import LigaReducer from "./liga";
+
+export default combineReducers({ LigaReducer });
